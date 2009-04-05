fbd0a2b22ae32e152d9adb239f1973ffc00974d9
Nikolaj Schumacher <git@nschum.de>
2009-04-05 16:39:46 +0200

    Added usage info.

--- full-ack.el
+++ full-ack.el
@@ -31,6 +31,11 @@
 ;; (autoload 'ack-same "full-ack" nil t)
 ;; (autoload 'ack "full-ack" nil t)
 ;;
+;; Run `ack' to search for all files and `ack-same' to search for files of the
+;; same type as the current buffer.
+;;
+;; `next-error' and `previous-error' can be used to jump to the matches.
+;;
 ;;; Change Log:
 ;;
 ;;    Initial release.
