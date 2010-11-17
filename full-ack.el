00f47f7c65ab019aa6b043e5424ef6ba3e13be95
Nikolaj Schumacher <git@nschum.de>
2010-11-17 21:00:56 +0100

    Bumped copyright year.

--- full-ack.el
+++ full-ack.el
@@ -1,6 +1,6 @@
 ;;; full-ack.el --- a front-end for ack
 ;;
-;; Copyright (C) 2009 Nikolaj Schumacher
+;; Copyright (C) 2009-2010 Nikolaj Schumacher
 ;;
 ;; Author: Nikolaj Schumacher <bugs * nschum de>
 ;; Version: 0.2.1
